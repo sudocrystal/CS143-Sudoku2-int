72cd714f0f4173f1bbc3298d6ef4191ddb6d75c4
Crystal Hess <chess@shoreline.edu>
2026-05-05 11:34:07 -0700

    Updates original Engine to look for data in proper location

--- SudokuGame.java
+++ SudokuGame.java
@@ -2,7 +2,7 @@ public class SudokuGame
 {
 	public static void main(String[] args)
 	{
-		SudokuBoard gameBoard = new SudokuBoard("data1.sdk");
+		SudokuBoard gameBoard = new SudokuBoard("boards/data1.sdk");
 		System.out.println(gameBoard);
       
       System.out.println("Is the current layout valid? " + gameBoard.isValid());
